231d736236f05eab9c7ffe693b0ece558a331c15
Digs-LS <diego.lucas.sousa@live.com>
2023-04-27 09:57:26 -0300

    feat: use feign rest client for service invocation form: alter code format and indentation

--- currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversion.java
+++ currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversion.java
@@ -11,10 +11,11 @@ public class CurrencyConversion {
 	private BigDecimal conversionMultiple;
 	private BigDecimal totalCalculatedAmount;
 	private String evironment;
-	
+
 	public CurrencyConversion() {
-		
+
 	}
+
 	public CurrencyConversion(Long id, String from, String to, BigDecimal quantity, BigDecimal conversionMultiple,
 			BigDecimal totalCalculatedAmount, String evironment) {
 		super();
@@ -26,45 +27,59 @@ public class CurrencyConversion {
 		this.totalCalculatedAmount = totalCalculatedAmount;
 		this.evironment = evironment;
 	}
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getFrom() {
 		return from;
 	}
+
 	public void setFrom(String from) {
 		this.from = from;
 	}
+
 	public String getTo() {
 		return to;
 	}
+
 	public void setTo(String to) {
 		this.to = to;
 	}
+
 	public BigDecimal getConversionMultiple() {
 		return conversionMultiple;
 	}
+
 	public void setConversionMultiple(BigDecimal conversionMultiple) {
 		this.conversionMultiple = conversionMultiple;
 	}
+
 	public BigDecimal getQuantity() {
 		return quantity;
 	}
+
 	public void setQuantity(BigDecimal quantity) {
 		this.quantity = quantity;
 	}
+
 	public BigDecimal getTotalCalculatedAmount() {
 		return totalCalculatedAmount;
 	}
+
 	public void setTotalCalculatedAmount(BigDecimal totalCalculatedAmount) {
 		this.totalCalculatedAmount = totalCalculatedAmount;
 	}
+
 	public String getEvironment() {
 		return evironment;
 	}
+
 	public void setEvironment(String evironment) {
 		this.evironment = evironment;
 	}
