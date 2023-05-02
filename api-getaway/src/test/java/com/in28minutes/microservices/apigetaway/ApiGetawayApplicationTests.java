cd8032a39cb6af4b608df764dd8188db9c33b2c4
Digs-LS <diego.lucas.sousa@live.com>
2023-05-02 17:51:32 -0300

    feat: sset up spring cloud api gateway

---
+++ api-getaway/src/test/java/com/in28minutes/microservices/apigetaway/ApiGetawayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.microservices.apigetaway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGetawayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
